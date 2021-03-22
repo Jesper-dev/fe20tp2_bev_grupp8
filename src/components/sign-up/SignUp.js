8918b56a9a96abf600afeec63ce27acaa70d1a50
JesperKYH <jesper.pettersson@student.kyh.se>
2021-03-22 11:44:02 +0100

    Hejsan

--- src/components/sign-up/SignUp.js
+++ src/components/sign-up/SignUp.js
@@ -201,17 +201,6 @@ class SignUpFormBase extends Component {
             return
         }
         this.setState({ [event.target.name]: event.target.value });
-
-
-        // function stringHasTheWhiteSpaceOrNot(value){
-        //     return value.indexOf(' ') >= 0;
-        //  }
-        //  var whiteSpace=stringHasTheWhiteSpaceOrNot("MyNameis John");
-        //     if(whiteSpace==true){
-        //        console.log("The string has whitespace");
-        //     } else {
-        //        console.log("The string does not have whitespace");
-        //  }
     };
 
     onChangeCheckbox = (event) => {
