b68cf80d3853e726dd80e4ddb5bd3065615ed8be
JesperKYH <jesper.pettersson@student.kyh.se>
2021-04-15 08:50:13 +0200

    Added start of achievments page

---
+++ src/components/profile/profile-achievments/ProfileAchievments.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfileAchievments = () => {
+    return (
+        <div>
+            <h1>This is the profile achievments page</h1>
+        </div>
+    )
+}
+
+export default ProfileAchievments
