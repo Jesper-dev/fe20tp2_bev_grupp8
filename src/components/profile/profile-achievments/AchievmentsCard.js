820d97a63f2a66d1b7e1e3f999b422a6585a34ed
JesperKYH <jesper.pettersson@student.kyh.se>
2021-04-16 10:07:38 +0200

    Fixed some stuff in achievments

--- src/components/profile/profile-achievments/AchievmentsCard.js
+++ src/components/profile/profile-achievments/AchievmentsCard.js
@@ -15,56 +15,87 @@ const AchievmentsCard = ({icon, name, desc, done, id}) => {
                 setChecked(true)
             }
         })
+        console.log(name)
+
         firebase.user(userData.uid).child('/achievments').on('value', (snapshot) => {
             const data = snapshot.val()
-            setAch(data)
+            let list = []
+            console.log(data)
+            for(const key in data) {
+                list.push(data[key])
+            }
+            setAch(list)
         })
-    }, [])
-    const onChangeFunc = () => {
-        let res = false;
-        res = checkIfShowed(id)
-        if(res === true) return;
+    }, [checked])
+    const onChangeFunc = (arr, id) => {
+        let cont = checkIfTrue(arr, id)
+        if(cont === true) return;
 
+        disableAchs(arr)
         setChecked(!checked)
 
         firebase.user(userData.uid).child(`/achievments/${id}`).update({
             show: !checked
         })
+
+        // toast.style.transform = 'scale(1)';
+        // setTimeout(() => {
+        //     toast.style.transform = 'scale(0)';
+        // }, 2500);
     }
 
-    const checkIfShowed = (id) => {
-        console.log(id)
-        if(id === "bitcoin") {
-            if(ach.millionaire.show === true) {
-                setChecked(false)
-                toast.style.transform = 'scale(1)';
-            setTimeout(() => {
-                toast.style.transform = 'scale(0)';
-            }, 2500);
-                return true;
+    const checkIfTrue = (arr, id) => {
+        arr.forEach((item) => {
+            if(item.id === id) {
+                if(item.show === true) {
+                    let cont = true;
+                    return cont;
+                }
             }
-        } else if(id === "millionaire") {
-            if(ach.bitcoin.show === true) {
-                setChecked(false)
-                toast.style.transform = 'scale(1)';
-            setTimeout(() => {
-                toast.style.transform = 'scale(0)';
-            }, 2500);
-                return true;
-            }
-        }
+        })
     }
+
+    const disableAchs = (arr) => {
+        arr.forEach((item) => {
+            firebase.user(userData.uid).child(`/achievments/${item.id}`).update({
+                show: false
+            })
+        })
+    }
+
+    // const checkIfShowed = (id) => {
+    //     console.log(id)
+    //     if(id === "bitcoin") {
+    //         if(ach.millionaire.show === true) {
+    //             setChecked(false)
+    //             toast.style.transform = 'scale(1)';
+    //         setTimeout(() => {
+    //             toast.style.transform = 'scale(0)';
+    //         }, 2500);
+    //             return true;
+    //         }
+    //     } else if(id === "millionaire") {
+    //         if(ach.bitcoin.show === true) {
+    //             setChecked(false)
+    //             toast.style.transform = 'scale(1)';
+    //         setTimeout(() => {
+    //             toast.style.transform = 'scale(0)';
+    //         }, 2500);
+    //             return true;
+    //         }
+    //     }
+    // }
     return (
         <>
             <AchievmentsCardWrapper done={done}>
                 {/* <img>{icon}</img> */}
                 <span>{name}</span>
                 <span>{desc}</span>
-                <input type="checkbox"  checked={checked} onChange={() => onChangeFunc()}/>
+                <input type="radio" id="radio" name="choose" checked={checked} onClick={() => onChangeFunc(ach, id)}/>
 
             </AchievmentsCardWrapper>
             <p className="toast" style={{ transform: 'scale(0)' }}>
-                You can only have on selected!
+                You selected to show {name} at your profile
             </p>
         </>
     )
